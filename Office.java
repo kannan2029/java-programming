593fe1930fda1a24be3df3f720a45de25010a58e
kannan <kannankaruppasamy778@gmail.com>
2023-01-25 15:33:11 +0530

    :first program

---
+++ Office.java
@@ -0,0 +1,11 @@
+class Office{
+String name,address;
+int age,mobile;
+public static void main(String arg[])
+{
+Office person1 = new Office();
+person1.applyAadhar("kannan","chennai",22);
+Office person2 = new Office();
+person2.applyAadhar("mafas","chennai",22);
+}
+
